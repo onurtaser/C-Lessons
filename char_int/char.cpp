03e75c92672033f80d4682360fd229d20b28d70b
onurtaser <126450684+onurtaser@users.noreply.github.com>
2023-02-28 15:29:16 +0300

    Add files via upload

---
+++ char_int/char.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+
+	int number[] = { 45,37,18,22,33 };
+	char name[] = "Onur";
+
+	std::cout << "My name is: " << name << "\nAnd my age is: " << number[3];
+
+	return 0;
+}
